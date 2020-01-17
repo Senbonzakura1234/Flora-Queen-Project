86569b518ed255c5dda550c1ff7dcfa3d702a3bc
Bằng Nguyễn <you@example.com>
2020-01-17 19:51:28 +0700

    Create CRUD Order.

---
+++ Flora Queen Project/Controllers/OrdersController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Flora_Queen_Project.Models;
+
+namespace Flora_Queen_Project.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.ApplicationOrders;
+            return View(orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var order = db.ApplicationOrders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.listOrderItem = db.OrderItems.Where(od => od.OrderId == id).ToList();
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, int OrderStatus)
+        {
+            var order = db.ApplicationOrders.Find(id);
+            order.OrderStatus = (Order.OrderStatusEnum) OrderStatus;
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+         
+            return View("Details/" + id);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var order = db.ApplicationOrders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var order = db.ApplicationOrders.Find(id);
+            db.ApplicationOrders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
