4afae464c4fc87a4631e51618d75f88eb8a7c3ee
Pham Anh Dung <anhdungpham090@gmail.com>
2020-01-18 21:57:29 +0700

    Add data to homepage, create partialmenu for homepage layout.

--- Flora Queen Project/Content/Boighor Homepage/js/custom.js
+++ Flora Queen Project/Content/Boighor Homepage/js/custom.js
@@ -6,4 +6,13 @@ $(function () {
             $(".loader-wrapper").fadeOut();
             $("html").css("overflow-y", "scroll");
         }, 1500);
+    $(".product__nav .nav-item.nav-link").click(function () {
+        $(".loader-wrapper").fadeIn();
+
+        setTimeout(
+            function () {
+                $(".loader-wrapper").fadeOut();
+                $("html").css("overflow-y", "scroll");
+            }, 1000);
+    });
 });
