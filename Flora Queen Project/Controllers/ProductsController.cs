2909028aa5d9dc1ab91cdf35679644755f2d3129
VMD121199 <minhducvu1211@gmail.com>
2020-02-02 00:15:05 +0700

    Done filter function, but cannot reload site after select element(using url still working)

--- Flora Queen Project/Controllers/ProductsController.cs
+++ Flora Queen Project/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
@@ -12,10 +14,105 @@ namespace Flora_Queen_Project.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string occasion, string type, string color, int? minAmount, int? maxAmount, int? viewMode, int? sortBy)
         {
-            var products = db.Products.Include(p => p.Color).Include(p => p.Occasion).Include(p => p.Type);
-            return View(products.ToList());
+            if (occasion == null)
+            {
+                occasion = "";
+            }
+
+            if (type == null)
+            {
+                type = "";
+            }
+
+            if (color == null)
+            {
+                color = "";
+            }
+
+            if (viewMode == null || viewMode > 1 || viewMode < 0)
+            {
+                viewMode = 0;
+            }
+
+            if (sortBy == null || sortBy > 5 || sortBy < 0)
+            {
+                sortBy = 0;
+            }
+            var data = db.Products.Where(p =>
+            p.TypeId.Contains(type) &&
+            p.OccasionId.Contains(occasion) &&
+            p.ColorId.Contains(color) &&
+            p.Price >= minAmount &&
+            p.Price <= maxAmount
+            ).ToList();
+            var listProduct = new List<Product>();
+
+            if (sortBy is (int)FilterEnum.Date)
+            {
+                var dataList = data.OrderByDescending(p => p.CreatedAt);
+                listProduct.AddRange(dataList);
+            }
+            else if (sortBy is (int)FilterEnum.NameAsc)
+            {
+                var dataList = data.OrderBy(p => p.Name);
+                listProduct.AddRange(dataList);
+            }
+            else if (sortBy is (int)FilterEnum.NameDesc)
+            {
+                var dataList = data.OrderByDescending(p => p.Name);
+                listProduct.AddRange(dataList);
+            }
+            else if (sortBy is (int)FilterEnum.PriceAsc)
+            {
+                var dataList = data.OrderBy(p => p.Price);
+                listProduct.AddRange(dataList);
+            }
+            else if (sortBy is (int)FilterEnum.PriceDesc)
+            {
+                var dataList = data.OrderByDescending(p => p.Price);
+                listProduct.AddRange(dataList);
+            }
+            else if (sortBy is (int)FilterEnum.SellRate)
+            {
+                var dataList = data.OrderByDescending(p => p.OrderItems.Count);
+                listProduct.AddRange(dataList);
+            }
+            else
+            {
+                var dataList = data.OrderByDescending(p => p.CreatedAt);
+                listProduct.AddRange(dataList);
+            }
+
+            ViewBag.sortBy = sortBy;
+
+            ViewBag.TotalItem = listProduct.Count;
+            ViewBag.Occasion = occasion;
+            ViewBag.Type = type;
+            ViewBag.Color = color;
+            ViewBag.minAmount = minAmount;
+            ViewBag.maxAmount = maxAmount;
+            ViewBag.viewMode = viewMode;
+
+            listProduct = listProduct.ToList();
+
+            return View(listProduct);
+        }
+        public enum FilterEnum
+        {
+            [Display(Name = "Sell rate")]
+            SellRate = 5,
+            [Display(Name = "Price Descending")]
+            PriceDesc = 4,
+            [Display(Name = "Price Ascending")]
+            PriceAsc = 3,
+            [Display(Name = "Name Descending")]
+            NameDesc = 2,
+            [Display(Name = "Name Ascending")]
+            NameAsc = 1,
+            [Display(Name = "Date")]
+            Date = 0
         }
 
         // GET: Products/Details/5
